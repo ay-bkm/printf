f8c5a23836f65d9349789278e9d5df6be38cc7c1
Ayoub <ay.belkacem@gmail.com>
2023-08-19 19:45:14 +0100

    _printf

--- _printf.c
+++ _printf.c
@@ -1,16 +1,78 @@
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character
+ * Return: Success: 1, error : -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
- *_printf - printf clone
- *@format: pointer to character string
- *Return: the number of characters printed
+ * print_char - prints a Char
+ * @args: argument list
+ * Return: Number of printed characters
+ */
+int print_char(va_list args)
+{
+	char c;
+
+	c = (char)va_arg(args, int);
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_string - prints string
+ * @args: argument list
+ * Return: Number of printed characters
+ */
+int print_string(va_list args)
+{
+	int len;
+	char *s;
+
+	len = 0;
+	s = va_arg(args, char *);
+
+	if (!s)
+		s = "(null)";
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * print_percent - prints '%'
+ * Return: Number of characters printed
+ */
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * _printf - printf clone
+ * @format: pointer to character string
+ *
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	char *s;
-	char c;
+	int len;
 	va_list args;
+
+	len = 0;
+
+	if (!format)
+		return (-1);
 
 	va_start(args, format);
 	while (*format)
@@ -18,20 +80,24 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			c = (*format == 'c') ? (char)va_arg(args, int) : '%';
-			s = (*format == 's') ? va_arg(args, char *) : NULL;
-			putchar(c);
-			len += (c != '%') ? 1 : 0;
-			while (s && *s)
+			if (!*format)
+				return (-1);
+			if (*format == 'c')
+				len += print_char(args);
+			else if (*format == 's')
+				len += print_string(args);
+			else if (*format == '%')
+				len += print_percent();
+			else
 			{
-				putchar(*s);
-				s++;
-				len++;
+				_putchar('%');
+				_putchar(*format);
+				len += 2;
 			}
 		}
 		else
 		{
-			putchar(*format);
+			_putchar(*format);
 			len++;
 		}
 		format++;
