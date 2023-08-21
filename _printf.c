f0a8b8cf2bd5e8f409545f636f9fcdb6638fe54e
Ayoub <ay.belkacem@gmail.com>
2023-08-21 15:54:17 +0100

    Update: each function has its own file

--- _printf.c
+++ _printf.c
@@ -1,64 +1,6 @@
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character
- * Return: Success: 1, error : -1
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_char - prints a Char
- * @args: argument list
- * Return: Number of printed characters
- */
-int print_char(va_list args)
-{
-	char c;
-
-	c = (char)va_arg(args, int);
-	_putchar(c);
-	return (1);
-}
-
-/**
- * print_string - prints string
- * @args: argument list
- * Return: Number of printed characters
- */
-int print_string(va_list args)
-{
-	va_list args;
-	int len;
-	char *s;
-
-	len = 0;
-	s = va_arg(args, char *);
-
-	if (!s)
-		s = "(null)";
-	while (*s)
-	{
-		_putchar(*s);
-		s++;
-		len++;
-	}
-	return (len);
-}
-
-/**
- * print_percent - prints '%'
- * Return: Number of characters printed
- */
-int print_percent(void)
-{
-	_putchar('%');
-	return (1);
-}
-
 /**
  * _printf - printf clone
  * @format: pointer to character string
---
+++ print_percent.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_percent - prints '%'
+ * Return: Number of characters printed
+ */
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
